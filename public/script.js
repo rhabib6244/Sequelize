346278dc61f920c819ee2d7ac03162831bd4ce90
Rida Habib <rhabib6244@gmail.com>
2021-03-27 15:06:30 -0400

    Added bulma table/final lab 8

--- public/script.js
+++ public/script.js
@@ -1,11 +1,23 @@
 async function windowActions() {
-
+  const table = document.querySelector(".table-content");
   const request = await fetch("/api/dining");
   const data = await request.json();
-
   console.log(request);
   console.log(data);
-  console.table(data);
+
+  data["data"].forEach((item) => {
+    let counter = 1;
+    const html = data["data"].map((hall) => {
+      return `
+          <tr>
+            <th><span class='id'>${counter++}</span></th>
+            <td><span class='name'>${hall.hall_name}</span></td>
+            <td><span class='address'>${hall.hall_address}</span></td>
+          </tr>
+          `;
+    });
+    table.innerHTML = html.join("");
+  });
 }
 
 window.onload = windowActions;
